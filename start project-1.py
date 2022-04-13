958137aab16305e098db0fdb458bff51866420aa
D2SDarius <101285104+D2SDarius@users.noreply.github.com>
2022-04-13 18:23:52 +0300

    Add files via upload

---
+++ start project-1.py
@@ -0,0 +1,36 @@
+import time
+
+msg = input("Enter the message: ")
+n = input("How many times ?: ")
+
+nr = 0
+true = False
+for i in range(0,int(n)):
+  print(msg + '\n')
+  nr += 1
+  true = True
+  #time.sleep(0.25)
+
+
+while true == True :
+
+  if nr == n:
+    time.sleep(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
